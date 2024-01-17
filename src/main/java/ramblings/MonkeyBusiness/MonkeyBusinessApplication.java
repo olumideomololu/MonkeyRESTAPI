ed18e13e39a0dd037f29f9318af894a8af39dff8
olumideomololu <oaomololu@gmail.com>
2024-01-17 09:18:01 +0000

    initial commit

---
+++ src/main/java/ramblings/MonkeyBusiness/MonkeyBusinessApplication.java
@@ -0,0 +1,13 @@
+package ramblings.MonkeyBusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeyBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkeyBusinessApplication.class, args);
+	}
+
+}
